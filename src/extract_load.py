5329400e64aefa9db99ecca83ee1f0113cacf11e
Francilândio <francilandioo@gmail.com>
2024-11-09 11:40:03 -0300

    ajuste de parametro do ticker para 'simbolo'

--- src/extract_load.py
+++ src/extract_load.py
@@ -4,6 +4,8 @@ from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
 commodities = ['CL=F', 'GC=F', 'SI=F']
 
 DB_HOST = os.getenv('DB_HOST_PROD')
@@ -13,12 +15,12 @@ DB_USER = os.getenv('DB_USER_PROD')
 DB_PASS = os.getenv('DB_PASS_PROD')
 DB_SCHEMA = os.getenv('DB_SCHEMA_PROD')
 
-DATABASE_URL = f"posrgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
 
 def buscar_dados_commodities(simbolo, periodo='5d', intervalo='1d'):
-    ticker = yf.Ticker('CL=F')
+    ticker = yf.Ticker(simbolo)
     dados = ticker.history(period=periodo, interval=intervalo)[['Close']]
     dados['simbolo'] = simbolo
     return dados
@@ -35,4 +37,4 @@ def salvar_no_postgres(df, schema='public'):
 
 if __name__ == "__main__":
     dados_concatenados = buscar_todos_dados_commodities(commodities)
-    print(dados_concatenados)
+    salvar_no_postgres(dados_concatenados, schema='public')
